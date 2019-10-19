94afe5b0f37af6146d32ad22c5c9243202e36e66
rterres <rterres@ethz.ch>
2019-10-19 13:29:37 +0200

    Add knn

--- ML_Project_2/scripts/knn.m
+++ ML_Project_2/scripts/knn.m
@@ -1,9 +1,11 @@
+cent=load('nodesCenters.mat');
+centers=cent.centers;
 k=20;
 [idx, clusters] = kmeans(centers,k);
 figure;
 cmap = hsv(k);
 for i=1:k
     scatter3(centers(idx==i,1),centers(idx==i,2),centers(idx==i,3),'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor',cmap(i,:));
-    scatter3(clusters(i,1),clusters(i,2),centers(i,3),'MarkerEdgeColor', cmap(i,:),'MarkerFaceColor',cmap(i,:));
+    scatter3(clusters(i,1),clusters(i,2),clusters(i,3),'*', 'MarkerEdgeColor', cmap(i,:));
     hold on;
 end
